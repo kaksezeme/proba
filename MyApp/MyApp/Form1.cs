ce1c359eaf0c96f46ba8c25f93d3fd5b5bf40c34
kaksezeme <pvlahovic@outlook.com>
2017-04-16 20:45:35 +0200

    Promjena1

--- MyApp/MyApp/Form1.cs
+++ MyApp/MyApp/Form1.cs
@@ -16,5 +16,12 @@ namespace MyApp
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            label1.Text = rand.Next(1, 10).ToString();
+        }
     }
 }
